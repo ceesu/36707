8bc42d79fe7de181c1d365a98f2f6de81988ae77
ceesu <qsu1@andrew.cmu.edu>
2019-09-14 17:06:23 -0400

    started hw3

---
+++ code/19_09_14_hw3.R
@@ -0,0 +1,17 @@
+##########################################
+##### HW3 data
+# Weisberg, Applied Linear Regression, 3rd ed., problem 5.5, page 113. 
+# https://onlinelibrary.wiley.com/doi/book/10.1002/0471704091
+# Cathy Su
+
+##########################################
+install.packages("alr3")
+library(alr3)
+help(snowgeese)
+
+##########################################
+# scatterplot
+
+
+##########################################
+# scatterplot
