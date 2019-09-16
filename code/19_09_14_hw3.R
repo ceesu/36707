32b95cdf48991891b568310ea7f8415c34ac0d5a
ceesu <qsu1@andrew.cmu.edu>
2019-09-15 21:15:42 -0400

    did minimal work

--- code/19_09_14_hw3.R
+++ code/19_09_14_hw3.R
@@ -5,13 +5,31 @@
 # Cathy Su
 
 ##########################################
-install.packages("alr3")
+# install.packages("alr3")
 library(alr3)
 help(snowgeese)
-
+head(snowgeese)
 ##########################################
 # scatterplot
-
-
+pairs(snowgeese, pch = 19, lower.panel = NULL)
+model1<- lm(photo~obs1,data=snowgeese)
+summary(model1)
 ##########################################
 # scatterplot
+model2<- lm(sqrt(photo)~sqrt(obs1),data=snowgeese)
+summary(model2)
+
+# square root: stabilize the error variance
+model3<- lm(sqrt(photo)~sqrt(obs1),data=snowgeese)
+summary(model2)
+
+# square root: stabilize the error variance
+model3<- lm(sqrt(photo)~sqrt(obs1),data=snowgeese)
+
+# obs1x sigma^2 variance
+
+# average
+summary(lm(sqrt(photo)~sqrt(obs2),data=snowgeese))
+
+# diff
+summary(lm(sqrt(photo)~sqrt(obs2),data=snowgeese))
